ace6fa04d7914bc9c748172924a687c271feb374
aqazix <pedrorrdesousa97@gmail.com>
2020-11-23 17:21:59 -0300

    Add default options

--- printElements.js
+++ printElements.js
@@ -1,49 +1,44 @@
-async function printElements(options) {
-    let outprint = document.createElement("div");
-    outprint.setAttribute("class", "outprint");
-    document.querySelector("body").append(outprint);
-  
-    const targets = options.targets;
-    const tags = options.tags.toString();
-    const willNotPrint = options.willNotPrint.replace(".", "");
-    let promises = [];
-    let html = [];
-  
-    targets.forEach(target => {
-      promises.push(fetch(target).then(response => {
-        return response.text();
-      }).then(string => {
-        // Fetches the text and parses to HTML
-        html.push(new DOMParser().parseFromString(string, "text/html"));
-      }));
-    });
-  
-    Promise.all(promises).then(() => {
-      let elem = [];
-      html.forEach(page => {
-        page.querySelector("main").querySelectorAll(tags).forEach(item => {
-          // Creates a copy of all relevant elements
-          elem.push(item.cloneNode(true));
-        });
-  
-        // Removes the included tags that contain the class that excludes from printing (defined in JSON and assigned in variable willNotPrint).
-        elem = elem.filter(item => !item.classList.contains(willNotPrint));
-  
-        // Append the new nodes to outprint
-        elem.forEach(item => {
-          document.querySelector(".outprint").append(item);
-        });
-      });
+const printElements = options => {
+    let outprint = document.createElement("div")
+    outprint.classList.add("outprint")
+    document.querySelector("body").append(outprint)
+
+    let { targets, tags, willNotPrint, wrapper } = options ? options : {}
+
+    targets = targets && targets.length > 0
+        ? targets
+        : ["index.html"]
+    tags = tags && tags.length > 0
+        ? tags.toString()
+        : "h1, h2, h3, h4, h5, h6, p, ul, table, small, img.print, svg.print, figcaption"
+    willNotPrint = willNotPrint
+        ? willNotPrint.replace(".", "")
+        : ""
+    wrapper = wrapper
+        ? wrapper
+        : "main"
+
+    let html = []
+
+    Promise.all(targets.map(target => fetch(target)
+        .then(response => response.text())
+        .then(string => html.push(new DOMParser().parseFromString(string, "text/html")))
+    )).then(() => {
+        html.forEach(page => {
+            let elem = [...page.querySelector(wrapper).querySelectorAll(tags)]
+                .map(item => item.cloneNode(true))
+                .filter(item => !item.classList.contains(willNotPrint))
+
+            elem.forEach(item => document.querySelector(".outprint").append(item))
+        })
+
+        setTimeout(() => {
+            window.print(document.querySelector(".outprint"))
+        }, 125)
     }).then(() => {
-      setTimeout(() => {
-        // Opens the print window to print the current document.
-        window.print(document.querySelector(".outprint"));
-      }, 125);
-    }).then(() => {
-      // Removes and erases the data inside the current outprint.
-      setTimeout(() => {
-        outprint.remove();
-        outprint.innerHTML = "";
-      }, 250);
-    });
-  }
+        setTimeout(() => {
+            outprint.remove()
+            outprint.innerHTML = ""
+        }, 250)
+    })
+}
